fdbbcb8022d65499533700d4442bb21940896312
Rapnah <tranducuy15@gmail.com>
2023-10-17 21:25:42 +0700

    learning

--- Control_Student/Update_Student.cpp
+++ Control_Student/Update_Student.cpp
@@ -7,7 +7,7 @@ void Student::setID(uint8_t _id)
 
 void Student::setName(const char* _name)
 {
-	strcpy(this->_name = _name);
+	strcpy(this->_name, _name);
 }
 
 void Student::setBrith(uint8_t _day, uint8_t _month, uint8_t _year)
