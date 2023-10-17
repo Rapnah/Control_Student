837e8bf362985f44b947fdd2dcc52600253b3040
Rapnah <tranducuy15@gmail.com>
2023-10-17 18:14:30 +0700

    Learning Build Project

--- Control_Student/Main.cpp
+++ Control_Student/Main.cpp
@@ -22,22 +22,31 @@ void main()
 		switch (choose)
 		{
 		case 1:
-			cout << "1. Add Student\n";
-			cout << "2. Remove Student\n";
-			cout << "3. Exit\n";
-			cin >> choose;
+			do
+			{
+				cout << "1. Add Student\n";
+				cout << "2. Remove Student\n";
+				cout << "3. Exit\n";
+				cin >> choose;
+			} while (choose != 3);
 			break;
 		case 2:
-			cout << "1. Search Student by Name\n";
-			cout << "2. Search Student by ID\n";
-			cout << "3. Exit\n";
-			cin >> choose;
+			do
+			{
+				cout << "1. Search Student by Name\n";
+				cout << "2. Search Student by ID\n";
+				cout << "3. Exit\n";
+				cin >> choose;
+			} while (choose != 3);
 			break;
 		case 3:
-			cout << "1. Sort Student by GPA\n";
-			cout << "2. Sort Student by Name\n";
-			cout << "3. Exit\n";
-			cin >> choose;
+			do
+			{
+				cout << "1. Sort Student by GPA\n";
+				cout << "2. Sort Student by Name\n";
+				cout << "3. Exit\n";
+				cin >> choose;
+			} while (choose != 3);
 		case 4:
 		case 5:
 		default:
